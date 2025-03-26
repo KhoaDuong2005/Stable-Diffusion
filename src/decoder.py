ddbf4f62329a0408a7e4b5c12be0b0352553c1d7
Elton <khoa.dhaminh723@gmail.com>
2025-03-26 16:13:50 -0700

    Add VAE_Decoder

--- src/decoder.py
+++ src/decoder.py
@@ -65,4 +65,68 @@ class VAE_AttentionBlock(nn.Module):
 
         x += residue
         
+        return x	
+
+
+class VAE_Decoder(nn.Sequential):
+    def __init__(self):
+        super().__init__(
+            nn.Conv2d(4, 4, kernel_size=1, padding=0),
+
+            nn.Conv2d(4, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 512),
+
+            VAE_AttentionBlock(512),
+
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+
+            # (batch_size, 512, H/8, W/8) -> (batch_size, 512, H/4, W/4)
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+
+            # (batch_size, 512, H/4, W/4) -> (batch_size, 512, H/2, W/2)
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+
+            # (batch_size, 512, H/2, W/2) -> (batch_size, 256, H/2, W/2)
+            VAE_ResidualBlock(512, 256),
+            VAE_ResidualBlock(256, 256),
+            VAE_ResidualBlock(256, 256),
+
+            # (batch_size, 256, H/2, W/2) -> (batch_size, 256, H, W)
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+
+            # (batch_size, 256, H, W) -> (batch_size, 128, H, W)
+            VAE_ResidualBlock(256, 128),
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),
+
+            nn.GroupNorm(32, 128), # GroupNorm divide 128 into groups of 32
+
+            nn.SiLU(),
+
+            # (batch_size, 128, H, W) -> (batch_size, 3, W, H)
+            nn.Conv2d(128, 3, kernel_size=3, padding=1)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x (batch_size, 4, H/8, W/8)
+        X /= 0.18215
+
+        for module in self:
+            x = module(x)
+
+        # batch_size, 3, H, W
         return x
