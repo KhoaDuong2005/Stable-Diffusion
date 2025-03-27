acfb2ed41358636dd64b632fa20d50e54f3342ea
Elton <khoa.dhaminh723@gmail.com>
2025-03-27 02:27:07 -0700

    Fixes import, improves residual block, and corrects bugs
    Updates import to use SelfAttention for consistency. Adjusts the
    residual layer kernel size in VAE_ResidualBlock for better
    performance. Corrects indentation errors and fixes a typo in
    VAE_Decoder forward method to ensure proper variable usage.
    
    Improves code clarity and robustness.

--- src/decoder.py
+++ src/decoder.py
@@ -1,7 +1,7 @@
 import torch
 from torch.nn import functional as F
 import torch.nn as nn
-from attention import self_attention_heads
+from attention import SelfAttention
 
 class VAE_ResidualBlock(nn.Module):
     def __init__(self, input_channel, output_channel):
@@ -15,30 +15,30 @@ class VAE_ResidualBlock(nn.Module):
         if input_channel == output_channel:
             self.residual_layer = nn.Identity()
         else:
-            self.residual_layer = nn.Conv2d(input_channel, output_channel, kernel_size=3, padding=0)
+            self.residual_layer = nn.Conv2d(input_channel, output_channel, kernel_size=1, padding=0)
 
-        def forward(self, x: torch.Tensor) -> torch.Tensor:
-            # x (batch_size, input_channels, H, W)
-            
-            residue = x
-            
-            x = self.groupnorm_1(x)
-            x = F.silu(x)
-            x = self.conv_1(x)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x (batch_size, input_channels, H, W)
+        
+        residue = x
+        
+        x = self.groupnorm_1(x)
+        x = F.silu(x)
+        x = self.conv_1(x)
 
-            x = self.groupnorm_2(x)
-            x = F.silu(x)
-            x = self.conv_2(x)
+        x = self.groupnorm_2(x)
+        x = F.silu(x)
+        x = self.conv_2(x)
 
-            x = x + self.residual_layer(residue)
+        x = x + self.residual_layer(residue)
 
-            return x
+        return x
 
 
 
 class VAE_AttentionBlock(nn.Module):
     def __init__(self, channels):
-        super.__init__()
+        super().__init__()
         self.groupnorm = nn.GroupNorm(32, channels)
         self.attention = SelfAttention(1, channels)
 
@@ -123,7 +123,7 @@ class VAE_Decoder(nn.Sequential):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # x (batch_size, 4, H/8, W/8)
-        X /= 0.18215
+        x /= 0.18215
 
         for module in self:
             x = module(x)
