eedc34bc39ab5505d20d0ca1d65cdd2ad28e5b1a
Elton <khoa.dhaminh723@gmail.com>
2025-03-26 21:06:55 -0700

    Refactors SelfAttention and adds CrossAttention
    Modifies the forward method in SelfAttention to use input_shape instead of reshaping x
    Introduces the new class CrossAttention for calculating attention between latent and prompt sequences
    
    Relates to #123

--- src/attention.py
+++ src/attention.py
@@ -13,8 +13,10 @@ class SelfAttention(nn.Module):
 
         self.n_heads = n_heads #number of head
         self.d_head = d_embeddings // n_heads #size of each head (dimension)
+
     def forward(self, x, casual_mask=False):
-        batch_size, sequence_length, d_embeddings  = x.shape
+        input_shape = x.shape
+        batch_size, sequence_length, d_embeddings  = input_shape
 
         interim_shape = (batch_size, sequence_length, self.n_heads, self.d_head)
 
@@ -45,11 +47,56 @@ class SelfAttention(nn.Module):
         output = output.transpose(1, 2)
 
         # (batch_size, seq_len, H, dim / H) -> (batch_size, seq_len, dim)
-        output.reshape(x.shape)
+        output.reshape(input_shape)
         
         output = self.out_proj(output)
 
         return output
 
+
+class CrossAttention(nn.Module):
+    def __init__(self, n_heads, d_embeddings: int, d_cross: int, in_proj_bias=True, out_proj_bias=True):
+        super().__init__()
+        self.q_proj = nn.Linear(d_embeddings, d_embeddings, bias=in_proj_bias)
+        self.k_proj = nn.Linear(d_cross, d_embeddings, bias=in_proj_bias)
+        self.v_proj = nn.Linear(d_cross, d_embeddings, bias=in_proj_bias)
+
+        self.out_proj = nn.Linear(d_embeddings, d_embeddings, bias=out_proj_bias)
+
+        self.n_heads = n_heads
+        self.d_heads = d_embeddings // n_heads
+    
+    def forward(self, x, y):
+        # x = (latent) = (batch_size, seq_len_Q, dim_Q)
+        # y = (prompt) = (batch_size, seq_len_KV, dim_KQ) = batch_size, 77, 768
+        input_shape = x.shape
+        batch_size, sequence_length, d_embeddings = input_shape
+
+        interim_shape = (batch_size, -1, self.n_heads, self.d_embeddings)
+
+        q = self.q_proj(x)
+
+        k = self.k_proj(y)
+        v = self.v_proj(y)
+
+        q = q.view(interim_shape).transpose(1, 2)
+        q = k.view(interim_shape).transpose(1, 2)
+        q = v.view(interim_shape).transpose(1, 2)
+
+        weight = q @ k.transpose(-2, -1)
+
+        weight /=   math.sqrt(self.d_heads)
+
+        weight = F.softmax(weight, dim=-1)
+
+        output = weight @ v
+
+        output = output.transpose(1, 2).contiguous()
+
+        output = output.view(input_shape)
+
+        output = self.out_proj(output)
+
+        return output
 
 
